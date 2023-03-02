12d7844e99d05793e5c8c1fb390ed2ba960a9fd1
AJAYKASTHALA <ajay.kasthala@gmail.com>
2023-03-02 12:10:31 +0530

    My 12th commit

--- client/vite-project/src/pages/Home.jsx
+++ client/vite-project/src/pages/Home.jsx
@@ -256,7 +256,33 @@ const Home = () => {
   const [searchText, setSearchText] = useState('');
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
-
+ //this one changed
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     setLoading(true);
+  
+  //     try {
+  //       const response = await fetch('https://dall-e-7njy.onrender.com/api/v1/post', {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //       });
+  
+  //       if (response.ok) {
+  //         const result = await response.json();
+  //         setAllPosts(result.data.reverse());
+  //       }
+  //     } catch (err) {
+  //       alert(err);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  
+  //   fetchPosts();
+  // }, [])
+  
   const fetchPosts = async () => {
     setLoading(true);
 
@@ -282,6 +308,10 @@ const Home = () => {
   useEffect(() => {
     fetchPosts();
   }, []);
+
+  // useEffect(() => {
+  //   fetchPosts();
+  // }, []);
 
   const handleSearchChange = (e) => {
     clearTimeout(searchTimeout);
