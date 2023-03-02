3a83115c8a49c2f1f06d39e869dcce9bb52890f2
AJAYKASTHALA <ajay.kasthala@gmail.com>
2023-03-02 13:58:06 +0530

    My 15th commit

--- client/vite-project/src/pages/Home.jsx
+++ client/vite-project/src/pages/Home.jsx
@@ -287,7 +287,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://dall-e-7njy.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
